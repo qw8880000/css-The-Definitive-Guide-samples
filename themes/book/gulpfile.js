948c8195df2b5faa45518bae64ebe1e75e2e18fb
qw8880000 <qw8880000@126.com>
2018-09-02 11:41:58 +0800

    [update] scss

--- themes/book/gulpfile.js
+++ themes/book/gulpfile.js
@@ -1,6 +1,8 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
 var sourcemaps = require('gulp-sourcemaps');
+var postcss = require('gulp-postcss');
+var autoprefixer = require('autoprefixer');
 
 //
 // sass convert to css
@@ -9,6 +11,19 @@ gulp.task('sass', function () {
   return gulp.src('./source/_scss/app.scss')
     .pipe(sourcemaps.init())
     .pipe(sass({outputStyle: 'expanded'}).on('error', sass.logError))
+    .pipe(postcss([autoprefixer()]))
     .pipe(sourcemaps.write('.'))
     .pipe(gulp.dest('./source/css'));
 });
+
+//
+// watch
+//
+gulp.task('watch', function () {
+  gulp.watch('./source/_scss/**/*.scss', ['sass']);
+});
+
+gulp.task('default', [
+  'sass',
+  'watch'
+]);
